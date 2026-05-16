d7c41bb61a42125ea25996ff61bb1f02d0b29975
vanya <mitrofanovvana20082@gmail.com>
2026-05-16 20:31:33 +0200

    feat: replace inline search form with SearchTaskForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import AddTaskForm from "./components/AddTaskForm.jsx";
+import SearchTaskForm from "./components/SearchTaskForm.jsx";
 
 const App = () => {
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
-      <form className="todo__form">
-        <div className="todo__field field">
-          <label
-            className="field__label"
-            htmlFor="search-task"
-          >
-            Search task
-          </label>
-          <input
-            className="field__input"
-            id="search-task"
-            placeholder=" "
-            autoComplete="off"
-            type="search"
-          />
-        </div>
-      </form>
+      <SearchTaskForm />
       <div className="todo__info">
         <div className="todo__total-tasks">Total tasks: <span>0</span></div>
         <button className="todo__delete-all-button" type="button">Delete all</button>
